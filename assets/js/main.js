69871f40c23f383719c7a1977dacd3111cabda23
lavanyaks <lavanya.singh24@gmail.com>
2018-02-11 16:19:32 -0500

    Initial commit - left meets right inc.

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    var showMenu = document.getElementsByClassName('menu-icon');
+    var closeMenu = document.getElementsByClassName('menu-close');
+    var navMenu = document.getElementsByClassName('slide-header');
+    var mainClass = document.getElementsByClassName('page-content');
+
+    showMenu[0].addEventListener('click', function () {
+        if (navMenu[0].classList.contains('open')) {
+            navMenu[0].classList.remove('open');
+            mainClass[0].classList.remove('open');
+        }
+        else {
+            navMenu[0].className += " open";
+            mainClass[0].classList += " open";
+        }
+    });
+
+    closeMenu[0].addEventListener('click', function () {
+        navMenu[0].classList.remove('open');
+    });
+};
